1767f42d3fe6e82d94fd8b30c81cdb7d6e96840d
Pranit Sah <157494834+pksaaho@users.noreply.github.com>
2025-04-13 13:24:39 +0530

    Add files via upload

---
+++ Blood-Bank-master/routes/inventoryRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const authMiddelware = require("../middlewares/authMiddelware");
+const {
+  createInventoryController,
+  getInventoryController,
+  getDonarsController,
+  getHospitalController,
+  getOrgnaisationController,
+  getOrgnaisationForHospitalController,
+  getInventoryHospitalController,
+  getRecentInventoryController,
+} = require("../controllers/inventoryController");
+
+const router = express.Router();
+
+//routes
+// ADD INVENTORY || POST
+router.post("/create-inventory", authMiddelware, createInventoryController);
+
+//GET ALL BLOOD RECORDS
+router.get("/get-inventory", authMiddelware, getInventoryController);
+
+//GET RECENT BLOOD RECORDS
+router.get("/get-recent-inventory", authMiddelware, getRecentInventoryController);
+
+//GET HOSPITAL BLOOD RECORDS
+router.post(
+  "/get-inventory-hospital",
+  authMiddelware,
+  getInventoryHospitalController
+);
+
+//GET DONAR RECORDS
+router.get("/get-donars", authMiddelware, getDonarsController);
+
+//GET HOSPITAL RECORDS
+router.get("/get-hospitals", authMiddelware, getHospitalController);
+
+//GET Organisation RECORDS
+router.get("/get-organisation", authMiddelware,getOrgnaisationController);
+
+//GET Organisation RECORDS
+router.get("/get-organisation-for-hospital", authMiddelware,getOrgnaisationForHospitalController);
+module.exports = router;
